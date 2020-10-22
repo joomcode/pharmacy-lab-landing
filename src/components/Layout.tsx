68eb68bb41e9a4f6763a8cf09d0bdad1335e1a1d
Ilya Lebedev <bacherko@gmail.com>
2020-10-22 18:43:39 +0300

    PB-158: add meta tags

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,6 +12,8 @@ export const Layout = ({title, children}: Props) => (
       <meta charSet='utf-8' />
       <title>{title}</title>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta property='og:title' key='og:title' content='Joom Pharm Solutions' />
+      <meta property='og:image' key='og:image' content='/cover_narrow.jpg' />
       <link rel='icon' href='/favicon.png' />
       <link href='https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500&display=swap' rel='stylesheet' />
     </Head>
